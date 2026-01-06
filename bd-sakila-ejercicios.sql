aed9742809bceab5f139b21be13ac04a52f2e6a3
beroncarla <beroncarlaviedma@gmail.com>
2026-01-06 18:43:48 -0300

    add select, modify readme

--- bd-sakila-ejercicios.sql
+++ bd-sakila-ejercicios.sql
@@ -30,3 +30,23 @@ from film
 inner join film_category on (film.film_id = film_category.film_id)      
 inner join category on (film_category.category_id = category.category_id)
 where category.name = 'Action';
+
+/¨comentario: listar los films de categorias 'Action', 'Music' and 'Sci-Fi' en los que 
+haya participado algun actor que también participo en un film de categoria 'Documentary en lenguaje English'*/
+select distinct film.title
+from film  
+inner join film_category on (film.film_id = film_category.film_id)      
+inner join category on (film_category.category_id = category.category_id)   
+inner join film_actor on (film.film_id = film_actor.film_id)
+inner join actor on (film_actor.actor_id = actor.actor_id)
+where category.name in ('Action', 'Music', 'Sci-Fi') and
+actor.actor_id in (
+    select distinct actor.actor_id
+    from actor
+    inner join film_actor on (actor.actor_id = film_actor.actor_id)
+    inner join film on (film.film_id = film_actor.film_id)
+    inner join film_category on (film.film_id = film_category.film_id)      
+    inner join category on (film_category.category_id = category.category_id)   
+    inner join language on (film.language_id = language.language_id)
+    where category.name = 'Documentary' and language.name = 'English'
+);
