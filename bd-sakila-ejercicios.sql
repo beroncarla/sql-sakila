bd7a75f6a2ed5b2434b3d9292e1faf1b1b521e25
beroncarla <beroncarlaviedma@gmail.com>
2026-01-06 17:41:03 -0300

    add inner

--- bd-sakila-ejercicios.sql
+++ bd-sakila-ejercicios.sql
@@ -10,12 +10,14 @@ from film;
 /**comentario: Ejercicio 3- listar todos los actores que participaron en la pelicula 'GLass Dying'  y que no participaron en la pelicula 'African Egg'*/
 select actor.first_name, actor.last_name
 from actor
-join film_actor on (actor.actor_id = film_actor.actor_id)
-join film on (film_actor.film_id = film.film_id)
+inner join film_actor on (actor.actor_id = film_actor.actor_id)
+inner join film on (film_actor.film_id = film.film_id)
 where (film.title = 'GLass Dying') and 
 actor.actor_id not in (
 	select actor.actor_id
     from actor
-    join film_actor on (actor.actor_id = film_actor.actor_id)
-    join film on (film.film_id = film_actor.actor_id)
+    inner join film_actor on (actor.actor_id = film_actor.actor_id)
+    inner join film on (film.film_id = film_actor.actor_id)
     where (film.title = 'African Egg'));
+
+
